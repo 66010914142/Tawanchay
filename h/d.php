e2f06bdadc4e44efa2a603d008ff4cb2fc23aacb
66010914142 <66010914142@msu.ac.th>
2026-02-03 12:18:37 +0700

    Add files via upload

---
+++ h/d.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+	?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ตะวันฉาย สวัสดิ์พาณิชย์</title>
+</head>
+
+<body>
+
+<h1>c.php</h1>
+
+<?php 
+	unset($_SESSION['name']);
+	unset($_SESSION['ืnickname']);
+?>
+</body>
+</html>
---
+++ h/index2.php
@@ -0,0 +1,20 @@
+<?php include_once("Check_login.php");?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>หน้าหลักAdmin - Dashboard</title>
+</head>
+
+<body>
+<h1>หน้าหลักAdmin - Dashboard</h1>
+<?php echo $_SESSION['aname']; ?>
+<ul>
+	<a href="index2.php"><li> หน้าหลักแอดมิน </li></a>
+	<a href="Products.php"><li> จัดการสินค้า </li></a>
+    <a href="orders.php"><li> จัดการออเดอร์ </li></a>
+    <a href="customers.php"><li> จัดการลูกค้า </li></a>
+    <a href="logout.php"><li> ออกจากระบบ </li></a>
+</ul>
+</body>
+</html>
