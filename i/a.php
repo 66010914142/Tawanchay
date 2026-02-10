e3e61622c106fc7e38b2751bb6c01892945f21e8
66010914142 <66010914142@msu.ac.th>
2026-02-10 09:43:24 +0700

    a

---
+++ i/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ตะวันฉาย สวัสดิ์พาณิชย์(ซัน)</title>
+</head>
+
+<body>
+<h1>งาน i </h1>
+<h1>ตะวันฉาย สวัสดิ์พาณิชย์(ซัน)</h1>
+</body>
+</html>
